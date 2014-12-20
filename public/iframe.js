c1fd0a0eb7ceb9d3d42753f49d3293439427305d
Romain Beauxis <toots@rastageeks.org>
2014-12-20 16:15:25 +0100

    Why not..

--- public/iframe.js
+++ public/iframe.js
@@ -1,3 +1,3 @@
 $(function () {
-  $("#map-canvas").html("<iframe src='http://meusu-tracker.herokuapp.com/' width='468' height='418' frameborder='0'></iframe>");
+  $("#map-canvas").html("<iframe src='//meusu-tracker.herokuapp.com/' width='468' height='418' frameborder='0'></iframe>");
 });
